64fd61006963271e837fa98c45aa189992f5771b
kdu2030 <kdu2030@gmail.com>
2024-09-08 17:11:41 -0400

    Initial commit

---
+++ src/main/java/com/kevin/prayerappservice/PrayerAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.prayerappservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrayerAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrayerAppServiceApplication.class, args);
+	}
+
+}
