1a30f1fef01fffd810c8ad2658aac5281a0cce56
kdu2030 <kdu2030@gmail.com>
2024-09-08 17:35:27 -0400

    Add General and Dev Application Properties, Add User Entity Class

---
+++ src/main/java/com/kevin/prayerappservice/entities/User.java
@@ -0,0 +1,18 @@
+package com.kevin.prayerappservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String passwordHash;
+}
