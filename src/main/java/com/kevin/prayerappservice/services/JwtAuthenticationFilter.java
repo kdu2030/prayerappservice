938e3fe570135d235087f6804189748e1d75dbe9
kdu2030 <kdu2030@gmail.com>
2024-09-13 23:03:03 -0400

    Add Generate Token Method

--- src/main/java/com/kevin/prayerappservice/services/JwtAuthenticationFilter.java
+++ src/main/java/com/kevin/prayerappservice/services/JwtAuthenticationFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -16,6 +17,13 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String AUTH_HEADER_NAME = "Authorization";
     private static final String BEARER_PREFIX = "Bearer ";
+
+    public JwtService jwtService;
+
+    @Autowired
+    public JwtAuthenticationFilter(JwtService jwtService){
+        this.jwtService = jwtService;
+    }
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
--- src/main/java/com/kevin/prayerappservice/services/JwtService.java
+++ src/main/java/com/kevin/prayerappservice/services/JwtService.java
@@ -3,23 +3,43 @@ package com.kevin.prayerappservice.services;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoder;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private final static int TOKEN_VALIDITY_LENGTH_MS = 1000 * 60 * 60 * 24 * 15;
 
     @Value("${jwt.signing-key}")
     private String signingKey;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken (UserDetails userDetails){
+        return generateToken(userDetails, new HashMap<>());
+    }
+
+    public String generateToken (UserDetails userDetails, Map<String, Object> extraClaims){
+        long currentSystemTime = System.currentTimeMillis();
+        return Jwts.builder()
+                .claims(extraClaims)
+                .subject(userDetails.getUsername())
+                .issuedAt(new Date(currentSystemTime))
+                .expiration(new Date(currentSystemTime + TOKEN_VALIDITY_LENGTH_MS))
+                .signWith(getSigningKey())
+                .compact();
     }
 
     private <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
