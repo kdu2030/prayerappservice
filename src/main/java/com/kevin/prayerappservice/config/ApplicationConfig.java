0179530403de8383b23d4514967bf144133bc029
kdu2030 <kdu2030@gmail.com>
2024-09-22 21:16:39 -0400

    Remove Unused Imports

--- src/main/java/com/kevin/prayerappservice/config/ApplicationConfig.java
+++ src/main/java/com/kevin/prayerappservice/config/ApplicationConfig.java
@@ -2,7 +2,6 @@ package com.kevin.prayerappservice.config;
 
 import com.kevin.prayerappservice.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
