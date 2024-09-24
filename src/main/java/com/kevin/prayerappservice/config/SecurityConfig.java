4e8fafdb5b4fc77c5c5e349382389475cf06013b
kdu2030 <kdu2030@gmail.com>
2024-09-23 20:18:44 -0400

    Finish Configuration for Filter Chain

---
+++ src/main/java/com/kevin/prayerappservice/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.kevin.prayerappservice.config;
+
+import com.kevin.prayerappservice.services.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                // This will ensure that authorization checks happen for every route except in whitelist
+                .authorizeHttpRequests((auths) -> auths
+                        .requestMatchers("/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                // We do not want to persist any data across requests to be compliant with REST
+                .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
