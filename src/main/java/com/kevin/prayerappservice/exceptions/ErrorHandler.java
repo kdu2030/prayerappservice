bbe687859216d706c700975c45d94176569e1d1f
kdu2030 <kdu2030@gmail.com>
2024-09-27 23:06:10 -0400

    Add Error Logging

--- src/main/java/com/kevin/prayerappservice/exceptions/ErrorHandler.java
+++ src/main/java/com/kevin/prayerappservice/exceptions/ErrorHandler.java
@@ -1,6 +1,8 @@
 package com.kevin.prayerappservice.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +11,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ErrorHandler {
+    private static final Logger logger = LoggerFactory.getLogger(ErrorHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Error> handleException(HttpServletRequest request, Exception exception){
+        logger.error("Generic Error occurred", exception);
+
         Error error = Error.builder()
                 .errorCode(ErrorCode.GENERIC_ERROR.getErrCode())
                 .message(ErrorCode.GENERIC_ERROR.getErrMessageKey())
@@ -22,6 +28,8 @@ public class ErrorHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<Error> handleValidationException(HttpServletRequest request, DataIntegrityViolationException exception){
+        logger.error("Data validation error occurred.", exception);
+
         Error error = Error.builder()
                 .errorCode(ErrorCode.DATA_VALIDATION_ERROR.getErrCode())
                 .message(exception.getMostSpecificCause().getLocalizedMessage())
