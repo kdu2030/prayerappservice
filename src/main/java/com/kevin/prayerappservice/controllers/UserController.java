c8d71abf4c306ec70beb08bf0a39b5ca1f8ce607
kdu2030 <kdu2030@gmail.com>
2024-09-29 16:15:57 -0400

    Remove Unused Imports

--- src/main/java/com/kevin/prayerappservice/controllers/UserController.java
+++ src/main/java/com/kevin/prayerappservice/controllers/UserController.java
@@ -1,20 +1,12 @@
 package com.kevin.prayerappservice.controllers;
 
-import com.kevin.prayerappservice.entities.Role;
-import com.kevin.prayerappservice.entities.User;
-import com.kevin.prayerappservice.entities.UserEmail;
-import com.kevin.prayerappservice.exceptions.DataValidationException;
 import com.kevin.prayerappservice.models.CreateUserRequest;
 import com.kevin.prayerappservice.models.UserDetails;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
 
 @Controller
 public class UserController implements UserApi {
