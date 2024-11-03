2a65b1463376b6bd29a6aab23469a0c3ab6a2b12
kdu2030 <kdu2030@gmail.com>
2024-11-03 14:18:17 -0500

    Fix Authentication Provider Warning

--- src/main/java/com/kevin/prayerappservice/config/ApplicationConfig.java
+++ src/main/java/com/kevin/prayerappservice/config/ApplicationConfig.java
@@ -1,36 +1,17 @@
 package com.kevin.prayerappservice.config;
 
-import com.kevin.prayerappservice.auth.AuthUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class ApplicationConfig {
-    private final AuthUserDetailsService authUserDetailsService;
-
-    @Autowired
-    public ApplicationConfig(AuthUserDetailsService authUserDetailsService) {
-        this.authUserDetailsService = authUserDetailsService;
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setUserDetailsService(authUserDetailsService);
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        return authenticationProvider;
     }
 
     @Bean
--- src/main/java/com/kevin/prayerappservice/config/SecurityConfig.java
+++ src/main/java/com/kevin/prayerappservice/config/SecurityConfig.java
@@ -1,14 +1,17 @@
 package com.kevin.prayerappservice.config;
 
+import com.kevin.prayerappservice.auth.AuthUserDetailsService;
 import com.kevin.prayerappservice.auth.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,14 +20,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationProvider authenticationProvider;
+    private final AuthUserDetailsService authUserDetailsService;
+    private final PasswordEncoder passwordEncoder;
 
-    private final String[] WHITELIST_URL_PATTERNS = { "/swagger-ui/**", "/v3/api-docs/**", "/api/v1/user/**" };
+    private static final String[] WHITELIST_URL_PATTERNS = {"/swagger-ui/**", "/v3/api-docs/**", "/api/v1/user/**"};
 
     @Autowired
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, AuthenticationProvider authenticationProvider) {
+    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter,
+                          AuthUserDetailsService authUserDetailsService, PasswordEncoder passwordEncoder) {
         this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-        this.authenticationProvider = authenticationProvider;
+        this.authUserDetailsService = authUserDetailsService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Bean
@@ -38,8 +44,15 @@ public class SecurityConfig {
                 )
                 // We do not want to persist any data across requests to be compliant with REST
                 .sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
+                .authenticationProvider(getAuthenticationProvider())
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
+    }
+
+    private AuthenticationProvider getAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(authUserDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+        return authenticationProvider;
     }
 }
