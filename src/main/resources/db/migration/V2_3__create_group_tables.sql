7bd9ef95dd68820ae50e2f882ec3541e318cd983
kdu2030 <kdu2030@gmail.com>
2024-11-07 22:38:26 -0500

    Add SQL for Prayer Group Role and Prayer Group User

--- src/main/resources/db/migration/V2_3__create_group_tables.sql
+++ src/main/resources/db/migration/V2_3__create_group_tables.sql
@@ -38,4 +38,26 @@ CREATE TABLE IF NOT EXISTS prayer_group (
     CONSTRAINT fk_image_file_id FOREIGN KEY (image_file_id) REFERENCES file(file_id)
 );
 
+CREATE TABLE IF NOT EXISTS prayer_group_role (
+    prayer_group_role_id INT PRIMARY KEY,
+    prayer_group_role_desc VARCHAR(255) NOT NULL
+);
+
+INSERT INTO prayer_group_role (prayer_group_role_id, prayer_group_role_desc) VALUES (1, 'admin');
+INSERT INTO prayer_group_role (prayer_group_role_id, prayer_group_role_desc) VALUES (2, 'user');
+
+CREATE SEQUENCE IF NOT EXISTS prayer_group_user_seq
+START 1
+INCREMENT 50;
+
+CREATE TABLE IF NOT EXISTS prayer_group_user (
+    prayer_group_user_id INT PRIMARY KEY DEFAULT nextval('prayer_group_user_seq'),
+    prayer_group_id INT NOT NULL,
+    user_id INT NOT NULL,
+    prayer_group_role_id INT NOT NULL,
+    CONSTRAINT fk_prayer_group_id FOREIGN KEY (prayer_group_id) REFERENCES prayer_group(prayer_group_id),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES app_user(user_id),
+    CONSTRAINT fk_prayer_group_role_id FOREIGN KEY (prayer_group_role_id) REFERENCES prayer_group_role (prayer_group_role_id)
+);
+
 COMMIT;
