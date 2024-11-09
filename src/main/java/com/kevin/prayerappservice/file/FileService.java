e6dfcbf0ffedb0ab720b0657aa8e8f143a673550
kdu2030 <kdu2030@gmail.com>
2024-11-09 10:48:28 -0500

    Set Up Http Client and Request

--- src/main/java/com/kevin/prayerappservice/file/FileService.java
+++ src/main/java/com/kevin/prayerappservice/file/FileService.java
@@ -3,17 +3,33 @@ package com.kevin.prayerappservice.file;
 import com.kevin.prayerappservice.exceptions.DataValidationException;
 import com.kevin.prayerappservice.file.entities.File;
 import com.kevin.prayerappservice.file.entities.FileType;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 @Service
 public class FileService {
+    @Value("fileupload.url")
+    private String fileUploadBaseUrl;
 
     public File uploadFile(MultipartFile rawFile){
         FileType fileType = FileType.getFileTypeFromContentType(rawFile.getContentType());
         if(fileType == FileType.UNKNOWN){
             throw new DataValidationException(new String[] {"File type is not supported."});
         }
+
+        HttpClient client = HttpClient.newHttpClient();
+        URI fileUploadUrl = URI.create(fileUploadBaseUrl + "/file-upload");
+        HttpRequest fileUploadRequest = HttpRequest.newBuilder(fileUploadUrl)
+                .header(HttpHeaders.CONTENT_TYPE, rawFile.getContentType())
+                .header(HttpHeaders.ACCEPT, String.valueOf(MediaType.APPLICATION_JSON))
+                .build();
 
         return null;
     }
