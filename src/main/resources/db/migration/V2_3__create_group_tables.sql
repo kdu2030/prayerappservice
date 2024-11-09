f165c2a84a857af0256ad792dd92874ea0b2a368
kdu2030 <kdu2030@gmail.com>
2024-11-08 20:52:05 -0500

    Make Enums Strings

--- src/main/resources/db/migration/V2_3__create_group_tables.sql
+++ src/main/resources/db/migration/V2_3__create_group_tables.sql
@@ -1,15 +1,3 @@
-BEGIN TRANSACTION;
-
-CREATE TABLE IF NOT EXISTS file_type (
-    file_type_id INT PRIMARY KEY,
-    file_type_desc VARCHAR(255)
-);
-
-INSERT INTO
-    file_type (file_type_id, file_type_desc)
-VALUES (1, 'image');
-
-
 CREATE SEQUENCE IF NOT EXISTS file_seq
 START 1
 INCREMENT 50;
@@ -17,9 +5,8 @@ INCREMENT 50;
 CREATE TABLE IF NOT EXISTS file (
     file_id INT PRIMARY KEY DEFAULT nextval('file_seq'),
     file_name VARCHAR(255) NOT NULL,
-    file_url VARCHAR(255) NOT NULL UNIQUE,
-    file_type_id INT,
-    CONSTRAINT fk_file_type_id FOREIGN KEY (file_type_id) REFERENCES file_type(file_type_id)
+    file_url VARCHAR(255) NOT NULL,
+    file_type VARCHAR(255) NOT NULL
 );
 
 
@@ -38,14 +25,6 @@ CREATE TABLE IF NOT EXISTS prayer_group (
     CONSTRAINT fk_image_file_id FOREIGN KEY (image_file_id) REFERENCES file(file_id)
 );
 
-CREATE TABLE IF NOT EXISTS prayer_group_role (
-    prayer_group_role_id INT PRIMARY KEY,
-    prayer_group_role_desc VARCHAR(255) NOT NULL
-);
-
-INSERT INTO prayer_group_role (prayer_group_role_id, prayer_group_role_desc) VALUES (1, 'admin');
-INSERT INTO prayer_group_role (prayer_group_role_id, prayer_group_role_desc) VALUES (2, 'user');
-
 CREATE SEQUENCE IF NOT EXISTS prayer_group_user_seq
 START 1
 INCREMENT 50;
@@ -54,10 +33,7 @@ CREATE TABLE IF NOT EXISTS prayer_group_user (
     prayer_group_user_id INT PRIMARY KEY DEFAULT nextval('prayer_group_user_seq'),
     prayer_group_id INT NOT NULL,
     user_id INT NOT NULL,
-    prayer_group_role_id INT NOT NULL,
-    CONSTRAINT fk_prayer_group_id FOREIGN KEY (prayer_group_id) REFERENCES prayer_group(prayer_group_id),
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES app_user(user_id),
-    CONSTRAINT fk_prayer_group_role_id FOREIGN KEY (prayer_group_role_id) REFERENCES prayer_group_role (prayer_group_role_id)
+    prayer_group_role VARCHAR(255) NOT NULL,
+    CONSTRAINT fk_prayer_group_id FOREIGN KEY (prayer_group_id) REFERENCES prayer_group (prayer_group_id),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES app_user(user_id)
 );
-
-COMMIT;
