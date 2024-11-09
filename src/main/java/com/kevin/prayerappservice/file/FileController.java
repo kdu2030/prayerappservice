38b6f25bf8f61cc5ab4f57973cfe91a072da64bb
kdu2030 <kdu2030@gmail.com>
2024-11-09 10:29:22 -0500

    Add File Service

--- src/main/java/com/kevin/prayerappservice/file/FileController.java
+++ src/main/java/com/kevin/prayerappservice/file/FileController.java
@@ -1,6 +1,9 @@
 package com.kevin.prayerappservice.file;
 
 import com.kevin.prayerappservice.file.entities.File;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,9 +11,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileController implements FileApi{
+    private final FileService fileService;
+
+    @Autowired
+    public FileController(FileService fileService){
+        this.fileService = fileService;
+    }
+
     @Override
-    public ResponseEntity<File> uploadFile(MultipartFile file) {
-        return null;
+    public ResponseEntity<File> uploadFile(MultipartFile rawFile) {
+        File file = fileService.uploadFile(rawFile);
+        return new ResponseEntity<>(file, HttpStatus.OK);
     }
 
     @Override
--- src/main/java/com/kevin/prayerappservice/file/FileService.java
+++ src/main/java/com/kevin/prayerappservice/file/FileService.java
@@ -1,7 +1,21 @@
 package com.kevin.prayerappservice.file;
 
+import com.kevin.prayerappservice.exceptions.DataValidationException;
+import com.kevin.prayerappservice.file.entities.File;
+import com.kevin.prayerappservice.file.entities.FileType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileService {
+
+    public File uploadFile(MultipartFile rawFile){
+        FileType fileType = FileType.getFileTypeFromContentType(rawFile.getContentType());
+        if(fileType == FileType.UNKNOWN){
+            throw new DataValidationException(new String[] {"File type is not supported."});
+        }
+
+        return null;
+    }
+
 }
