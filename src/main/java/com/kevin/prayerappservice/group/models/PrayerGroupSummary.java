061815a0be0f4c00634358decca08191d3d62876
kdu2030 <kdu2030@gmail.com>
2024-12-08 16:28:02 -0500

    Simplify Prayer Group Summary to Record Class

--- src/main/java/com/kevin/prayerappservice/group/models/PrayerGroupSummary.java
+++ src/main/java/com/kevin/prayerappservice/group/models/PrayerGroupSummary.java
@@ -6,51 +6,14 @@ import com.kevin.prayerappservice.utils.ColorUtils;
 
 import java.util.Optional;
 
-public class PrayerGroupSummary {
-    private final int prayerGroupId;
-    private final String groupName;
-    private final String description;
-    private final String rules;
-    private final String color;
-    private final String imageUrl;
-
-    public PrayerGroupSummary(int prayerGroupId, String groupName, String description, String rules, String color, String imageUrl) {
-        this.prayerGroupId = prayerGroupId;
-        this.groupName = groupName;
-        this.description = description;
-        this.rules = rules;
-        this.color = color;
-        this.imageUrl = imageUrl;
-    }
-
-    public int getPrayerGroupId() {
-        return prayerGroupId;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getRules() {
-        return rules;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
+public record PrayerGroupSummary(int prayerGroupId, String groupName, String description, String rules, String color,
+                                 String imageUrl) {
 
 
     public static PrayerGroupSummary createPrayerGroupSummary(PrayerGroup prayerGroup) {
         Optional<File> imageFile = Optional.ofNullable(prayerGroup.getImageFile());
         String imageUrl = imageFile.map(File::getFileUrl).orElse(null);
+
         return new PrayerGroupSummary(prayerGroup.getPrayerGroupId(), prayerGroup.getGroupName(),
                 prayerGroup.getDescription(), prayerGroup.getRules(),
                 ColorUtils.colorIntToHexString(prayerGroup.getColor()), imageUrl);
