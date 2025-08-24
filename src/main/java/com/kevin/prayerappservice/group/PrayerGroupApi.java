98e196fd70fe40d08102c3cced0d3af9255a01e0
kdu2030 <kdu2030@gmail.com>
2025-08-24 17:53:19 -0400

    Add Prayer Group API Interface

---
+++ src/main/java/com/kevin/prayerappservice/group/PrayerGroupApi.java
@@ -0,0 +1,18 @@
+package com.kevin.prayerappservice.group;
+
+import com.kevin.prayerappservice.group.models.CreatePrayerGroupRequest;
+import com.kevin.prayerappservice.group.models.PrayerGroupModel;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "PrayerGroup API", description = "The PrayerGroup API")
+@RequestMapping("/api/prayergroup")
+public interface PrayerGroupApi {
+    @PostMapping(value = "", produces = {"application/json"}, consumes = { "application/json" })
+    @Operation(summary = "Creates prayer group")
+    ResponseEntity<PrayerGroupModel> createPrayerGroup(@RequestBody CreatePrayerGroupRequest createPrayerGroupRequest);
+}
