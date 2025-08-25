93d4c32c46a4e9b9ed3c98d6b1155602fa50092a
kdu2030 <kdu2030@gmail.com>
2025-08-24 23:06:53 -0400

    Clean Up Imports

--- src/main/java/com/kevin/prayerappservice/group/PrayerGroupJdbcRepositoryImpl.java
+++ src/main/java/com/kevin/prayerappservice/group/PrayerGroupJdbcRepositoryImpl.java
@@ -3,11 +3,8 @@ package com.kevin.prayerappservice.group;
 import com.kevin.prayerappservice.group.dtos.CreatePrayerGroupRequestDTO;
 import com.kevin.prayerappservice.group.dtos.CreatedPrayerGroupDTO;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
