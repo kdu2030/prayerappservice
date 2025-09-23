ae9fb17075915415bc66ab9e19031b1bfeaa6225
kdu2030 <kdu2030@gmail.com>
2025-09-22 20:09:17 -0400

    Add User Already Joined Validation

--- src/main/java/com/kevin/prayerappservice/join/JoinRequestService.java
+++ src/main/java/com/kevin/prayerappservice/join/JoinRequestService.java
@@ -2,8 +2,11 @@ package com.kevin.prayerappservice.join;
 
 import com.kevin.prayerappservice.exceptions.DataValidationException;
 import com.kevin.prayerappservice.group.PrayerGroupRepository;
+import com.kevin.prayerappservice.group.PrayerGroupUserRepository;
 import com.kevin.prayerappservice.group.constants.PrayerGroupErrorMessages;
 import com.kevin.prayerappservice.group.entities.PrayerGroup;
+import com.kevin.prayerappservice.group.entities.PrayerGroupUser;
+import com.kevin.prayerappservice.join.constants.JoinRequestErrorMessages;
 import com.kevin.prayerappservice.join.entities.JoinRequest;
 import com.kevin.prayerappservice.join.mappers.JoinRequestMapper;
 import com.kevin.prayerappservice.join.models.JoinRequestCreateRequest;
@@ -18,20 +21,27 @@ import java.util.Optional;
 public class JoinRequestService {
     private final JoinRequestRepository joinRequestRepository;
     private final PrayerGroupRepository prayerGroupRepository;
+    private final PrayerGroupUserRepository prayerGroupUserRepository;
     private final EntityManager entityManager;
     private final JoinRequestMapper joinRequestMapper;
 
-    public JoinRequestService(JoinRequestRepository joinRequestRepository, PrayerGroupRepository prayerGroupRepository, EntityManager entityManager, JoinRequestMapper joinRequestMapper) {
+    public JoinRequestService(JoinRequestRepository joinRequestRepository, PrayerGroupRepository prayerGroupRepository, EntityManager entityManager, JoinRequestMapper joinRequestMapper, PrayerGroupUserRepository prayerGroupUserRepository) {
         this.joinRequestRepository = joinRequestRepository;
         this.prayerGroupRepository = prayerGroupRepository;
         this.entityManager = entityManager;
         this.joinRequestMapper = joinRequestMapper;
+        this.prayerGroupUserRepository = prayerGroupUserRepository;
     }
 
     public JoinRequestModel createJoinRequest(int prayerGroupId, JoinRequestCreateRequest joinRequestCreateRequest){
         Optional<PrayerGroup> prayerGroupResponse = prayerGroupRepository.findById(prayerGroupId);
         if(prayerGroupResponse.isEmpty()){
             throw new DataValidationException(String.format(PrayerGroupErrorMessages.CANNOT_FIND_PRAYER_GROUP, prayerGroupId));
+        }
+
+        Optional<PrayerGroupUser> prayerGroupUserResponse = prayerGroupUserRepository.findByPrayerGroup_prayerGroupIdAndUser_userId(prayerGroupId, joinRequestCreateRequest.getUserId());
+        if(prayerGroupUserResponse.isPresent()){
+            throw new DataValidationException(JoinRequestErrorMessages.USER_ALREADY_JOINED);
         }
 
         User user = entityManager.getReference(User.class, joinRequestCreateRequest.getUserId());
---
+++ src/main/java/com/kevin/prayerappservice/join/constants/JoinRequestErrorMessages.java
@@ -0,0 +1,5 @@
+package com.kevin.prayerappservice.join.constants;
+
+public class JoinRequestErrorMessages {
+    public static final String USER_ALREADY_JOINED = "User already joined prayer group.";
+}
