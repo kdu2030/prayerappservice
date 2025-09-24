6535a4fc9b3bebf970709a9a3ffe15258260d1b7
kdu2030 <kdu2030@gmail.com>
2025-09-23 22:09:34 -0400

    Add Delete Join Requests Repository Method

--- src/main/java/com/kevin/prayerappservice/join/JoinRequestJdbcRepositoryImpl.java
+++ src/main/java/com/kevin/prayerappservice/join/JoinRequestJdbcRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.kevin.prayerappservice.join;
 
 import com.kevin.prayerappservice.join.dtos.JoinRequestDTO;
+import com.kevin.prayerappservice.join.dtos.JoinRequestDeleteQuery;
 import com.kevin.prayerappservice.join.dtos.JoinRequestQuery;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
@@ -22,5 +23,13 @@ public class JoinRequestJdbcRepositoryImpl implements JoinRequestJdbcRepository 
         JoinRequestQuery joinRequestQuery = new JoinRequestQuery(prayerGroupId);
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(joinRequestQuery);
         return jdbcTemplate.query(sql, params, new BeanPropertyRowMapper<>(JoinRequestDTO.class));
+    }
+
+    public void deleteJoinRequests(int prayerGroupId, List<Integer> joinRequestIds){
+        String sql = "CALL delete_join_requests(:prayerGroupId, :joinRequestIds);";
+        int[] joinRequestIdsArr = joinRequestIds.stream().mapToInt(Integer::intValue).toArray();
+        JoinRequestDeleteQuery joinRequestDeleteQuery = new JoinRequestDeleteQuery(prayerGroupId, joinRequestIdsArr);
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(joinRequestDeleteQuery);
+        jdbcTemplate.update(sql, params);
     }
 }
