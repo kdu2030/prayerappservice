b4224fb2f100722f102324f32ea9a05659ed9546
= <kdu2030@gmail.com>
2025-10-19 21:42:15 -0400

    Add SQL to Create Prayer Request Like Table and Prayer Request Comment Table

---
+++ src/main/resources/db/migration/V2_26__create_prayer_request_comment_table.sql
@@ -0,0 +1,13 @@
+CREATE SEQUENCE IF NOT EXISTS prayer_request_comment_seq
+START 1
+INCREMENT 50;
+
+CREATE TABLE IF NOT EXISTS prayer_request_comment(
+    prayer_request_comment_id INT NOT NULL DEFAULT nextval('prayer_request_comment_seq'),
+    comment TEXT NOT NULL,
+    prayer_request_id INT NOT NULL,
+    user_id INT NOT NULL,
+    submitted_date TIMESTAMPTZ NOT NULL,
+    CONSTRAINT fk_prayer_request_id FOREIGN KEY (prayer_request_id) REFERENCES prayer_request(prayer_request_id),
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES app_user(user_id)
+);
