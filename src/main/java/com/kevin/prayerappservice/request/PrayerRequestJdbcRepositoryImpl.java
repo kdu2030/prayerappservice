8ac631033e67cf437410d45b14f0e5445834dfa3
kdu2030 <kdu2030@gmail.com>
2025-10-28 18:50:00 -0400

    Add Get Prayer Requests

--- src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepositoryImpl.java
+++ src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepositoryImpl.java
@@ -2,10 +2,14 @@ package com.kevin.prayerappservice.request;
 
 import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateQuery;
 import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateResult;
+import com.kevin.prayerappservice.request.dtos.PrayerRequestGetQuery;
+import com.kevin.prayerappservice.request.dtos.PrayerRequestGetResult;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class PrayerRequestJdbcRepositoryImpl implements PrayerRequestJdbcRepository {
@@ -19,5 +23,11 @@ public class PrayerRequestJdbcRepositoryImpl implements PrayerRequestJdbcReposit
         String sql = "SELECT * FROM create_prayer_request(:requestTitle, :requestDescription, :createdDate, :expirationDate, :prayerGroupId, :userId);";
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(createQuery);
         return jdbcTemplate.queryForObject(sql, params, new BeanPropertyRowMapper<>(PrayerRequestCreateResult.class));
+    }
+
+    public List<PrayerRequestGetResult> getPrayerRequests(PrayerRequestGetQuery getQuery){
+        String sql = "SELECT * FROM get_prayer_request(:targetUserId, :prayerGroupIds, :creatorUserIds, :bookmarkedUserId, :includeExpired, :sortField, :sortDirection, :skip, :take);";
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(getQuery);
+        return jdbcTemplate.query(sql, params, new BeanPropertyRowMapper<>(PrayerRequestGetResult.class));
     }
 }
