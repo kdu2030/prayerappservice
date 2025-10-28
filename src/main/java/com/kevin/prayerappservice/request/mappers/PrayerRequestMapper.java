8a999f1c0f5d82feed7bdad33d9d7267f4a55e1e
kdu2030 <kdu2030@gmail.com>
2025-10-28 19:06:29 -0400

    Start on Prayer Request Get Result to Prayer Request Model Mapper

--- src/main/java/com/kevin/prayerappservice/request/mappers/PrayerRequestMapper.java
+++ src/main/java/com/kevin/prayerappservice/request/mappers/PrayerRequestMapper.java
@@ -1,14 +1,12 @@
 package com.kevin.prayerappservice.request.mappers;
 
 import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateResult;
+import com.kevin.prayerappservice.request.dtos.PrayerRequestGetResult;
 import com.kevin.prayerappservice.request.models.PrayerRequestCreateRequest;
 import com.kevin.prayerappservice.request.models.PrayerRequestModel;
 import com.kevin.prayerappservice.request.models.PrayerRequestPrayerGroupSummary;
 import com.kevin.prayerappservice.request.models.PrayerRequestUserSummary;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring")
 public interface PrayerRequestMapper {
@@ -34,6 +32,25 @@ public interface PrayerRequestMapper {
     @Mapping(constant = "0", target = "commentCount")
     PrayerRequestModel prayerRequestCreateResultToPrayerRequestModel(PrayerRequestCreateResult source);
 
+    @Mapping(source = "prayerGroupId", target = "prayerGroup.prayerGroupId")
+    @Mapping(source = "groupName", target = "prayerGroup.groupName")
+    @Mapping(source = "avatarFileId", target = "prayerGroup.avatarFile.mediaFileId")
+    @Mapping(source = "avatarFileName", target = "prayerGroup.avatarFile.fileName")
+    @Mapping(source = "avatarFileUrl", target = "prayerGroup.avatarFile.fileUrl")
+    @Mapping(source = "avatarFileType", target = "prayerGroup.avatarFile.fileType")
+    @Mapping(source = "userId", target = "user.userId")
+    @Mapping(source = "username", target = "user.username")
+    @Mapping(source = "fullName", target = "user.fullName")
+    @Mapping(source = "userFileId", target = "user.image.mediaFileId")
+    @Mapping(source = "userFileName", target = "user.image.fileName")
+    @Mapping(source = "userFileUrl", target = "user.image.fileUrl")
+    @Mapping(source = "userFileType", target = "user.image.fileType")
+    @Mapping(source = "userLikeId", target = "userLiked", qualifiedByName = "mapIntegerToBoolean")
+    @Mapping(source = "userCommentId", target = "userCommented", qualifiedByName = "mapIntegerToBoolean")
+    @Mapping(source = "userBookmarkId", target = "userBookmarked", qualifiedByName = "mapIntegerToBoolean")
+    @Mapping(source = "userPrayerSessionId", target = "userPrayed", qualifiedByName = "mapIntegerToBoolean")
+    PrayerRequestModel prayerRequestGetResultToPrayerRequestModel(PrayerRequestGetResult source);
+
     @AfterMapping
     default void setImagesToNull(@MappingTarget PrayerRequestModel prayerRequestModel){
         PrayerRequestPrayerGroupSummary prayerGroupSummary = prayerRequestModel.getPrayerGroup();
@@ -46,5 +63,10 @@ public interface PrayerRequestMapper {
         if(prayerRequestUserSummary.getImage().getMediaFileId() == null){
             prayerRequestUserSummary.setImage(null);
         }
+    }
+
+    @Named("mapIntegerToBoolean")
+    default boolean mapIntegerToBoolean(Integer value){
+        return value != null;
     }
 }
