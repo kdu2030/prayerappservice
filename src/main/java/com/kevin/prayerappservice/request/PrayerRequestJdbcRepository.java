737841edf55af03a36d1ac7aad24d6f01ee7ce2e
kdu2030 <kdu2030@gmail.com>
2025-10-28 18:50:31 -0400

    Add Get Prayer Requests

--- src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepository.java
+++ src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepository.java
@@ -2,7 +2,12 @@ package com.kevin.prayerappservice.request;
 
 import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateQuery;
 import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateResult;
+import com.kevin.prayerappservice.request.dtos.PrayerRequestGetQuery;
+import com.kevin.prayerappservice.request.dtos.PrayerRequestGetResult;
+
+import java.util.List;
 
 public interface PrayerRequestJdbcRepository {
     PrayerRequestCreateResult createPrayerRequest(PrayerRequestCreateQuery createQuery);
+    List<PrayerRequestGetResult> getPrayerRequests(PrayerRequestGetQuery getQuery);
 }
