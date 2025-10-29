a8a320bbd1903c3135b16b9b944e635d0cda1054
kdu2030 <kdu2030@gmail.com>
2025-10-29 11:39:18 -0400

    Add Get Prayer Requests Count

--- src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepository.java
+++ src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepository.java
@@ -1,13 +1,11 @@
 package com.kevin.prayerappservice.request;
 
-import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateQuery;
-import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateResult;
-import com.kevin.prayerappservice.request.dtos.PrayerRequestGetQuery;
-import com.kevin.prayerappservice.request.dtos.PrayerRequestGetResult;
+import com.kevin.prayerappservice.request.dtos.*;
 
 import java.util.List;
 
 public interface PrayerRequestJdbcRepository {
     PrayerRequestCreateResult createPrayerRequest(PrayerRequestCreateQuery createQuery);
     List<PrayerRequestGetResult> getPrayerRequests(PrayerRequestGetQuery getQuery);
+    PrayerRequestCountResult getPrayerRequestsCount(PrayerRequestCountQuery countQuery);
 }
--- src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepositoryImpl.java
+++ src/main/java/com/kevin/prayerappservice/request/PrayerRequestJdbcRepositoryImpl.java
@@ -1,9 +1,6 @@
 package com.kevin.prayerappservice.request;
 
-import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateQuery;
-import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateResult;
-import com.kevin.prayerappservice.request.dtos.PrayerRequestGetQuery;
-import com.kevin.prayerappservice.request.dtos.PrayerRequestGetResult;
+import com.kevin.prayerappservice.request.dtos.*;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -19,15 +16,24 @@ public class PrayerRequestJdbcRepositoryImpl implements PrayerRequestJdbcReposit
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    @Override
     public PrayerRequestCreateResult createPrayerRequest(PrayerRequestCreateQuery createQuery){
         String sql = "SELECT * FROM create_prayer_request(:requestTitle, :requestDescription, :createdDate, :expirationDate, :prayerGroupId, :userId);";
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(createQuery);
         return jdbcTemplate.queryForObject(sql, params, new BeanPropertyRowMapper<>(PrayerRequestCreateResult.class));
     }
 
+    @Override
     public List<PrayerRequestGetResult> getPrayerRequests(PrayerRequestGetQuery getQuery){
         String sql = "SELECT * FROM get_prayer_requests(:targetUserId, :prayerGroupIds, :creatorUserIds, :bookmarkedUserId, :includeExpired, :sortField, :sortDirection, :skip, :take);";
         BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(getQuery);
         return jdbcTemplate.query(sql, params, new BeanPropertyRowMapper<>(PrayerRequestGetResult.class));
+    }
+
+    @Override
+    public PrayerRequestCountResult getPrayerRequestsCount(PrayerRequestCountQuery countQuery){
+        String sql = "SELECT * FROM get_prayer_requests_count(:targetUserId, :prayerGroupIds, :creatorUserIds, :bookmarkedUserId, :includeExpired);";
+        BeanPropertySqlParameterSource params = new BeanPropertySqlParameterSource(countQuery);
+        return jdbcTemplate.queryForObject(sql, params, new BeanPropertyRowMapper<>(PrayerRequestCountResult.class));
     }
 }
