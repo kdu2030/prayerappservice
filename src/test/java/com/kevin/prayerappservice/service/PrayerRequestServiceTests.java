9cd11ac202d9f9676b9c0fd258c24a26d58504c0
kdu2030 <kdu2030@gmail.com>
2025-10-29 16:46:22 -0400

    Add Create Prayer Request Valid Request

--- src/test/java/com/kevin/prayerappservice/service/PrayerRequestServiceTests.java
+++ src/test/java/com/kevin/prayerappservice/service/PrayerRequestServiceTests.java
@@ -4,12 +4,20 @@ import com.kevin.prayerappservice.auth.JwtService;
 import com.kevin.prayerappservice.exceptions.DataValidationException;
 import com.kevin.prayerappservice.group.PrayerGroupRepository;
 import com.kevin.prayerappservice.group.PrayerGroupUserRepository;
+import com.kevin.prayerappservice.group.constants.PrayerGroupRole;
 import com.kevin.prayerappservice.group.constants.VisibilityLevel;
 import com.kevin.prayerappservice.group.entities.PrayerGroup;
+import com.kevin.prayerappservice.group.entities.PrayerGroupUser;
+import com.kevin.prayerappservice.request.PrayerRequestJdbcRepositoryImpl;
 import com.kevin.prayerappservice.request.PrayerRequestService;
+import com.kevin.prayerappservice.request.dtos.PrayerRequestCreateResult;
 import com.kevin.prayerappservice.request.models.PrayerRequestCreateRequest;
+import com.kevin.prayerappservice.request.models.PrayerRequestModel;
+import com.kevin.prayerappservice.user.UserRepository;
+import com.kevin.prayerappservice.user.entities.Role;
+import com.kevin.prayerappservice.user.entities.User;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +27,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringBootTest
@@ -29,27 +37,67 @@ public class PrayerRequestServiceTests {
     private JwtService jwtService;
 
     @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
     private PrayerGroupRepository prayerGroupRepository;
 
     @Autowired
     private PrayerGroupUserRepository prayerGroupUserRepository;
+
+    @MockBean
+    private PrayerRequestJdbcRepositoryImpl prayerRequestJdbcRepository;
 
     @Autowired
     private PrayerRequestService prayerRequestService;
 
     @Test
     @DirtiesContext
-    public void createPrayerRequest_nonMemberAttemptsToCreate_ThrowsException(){
+    public void createPrayerRequest_nonMemberAttemptsToCreate_ThrowsException() {
         Mockito.when(jwtService.extractTokenFromAuthHeader(anyString())).thenReturn("mockToken");
         Mockito.when(jwtService.extractUserId("mockToken")).thenReturn(1);
 
-        PrayerGroup mockPrayerGroup = new PrayerGroup("Gryzzl Prayer Group", "A prayer group for Gryzzl", null, VisibilityLevel.PRIVATE, null, null);
+        PrayerGroup mockPrayerGroup = new PrayerGroup("Gryzzl Prayer Group", "A prayer group for Gryzzl", null,
+                VisibilityLevel.PRIVATE, null, null);
         prayerGroupRepository.save(mockPrayerGroup);
 
-        PrayerRequestCreateRequest createRequest = new PrayerRequestCreateRequest(1, "Suffering from headaches", "Getting headaches, pray for healing", LocalDateTime.now().plusMonths(1), LocalDateTime.now());
+        PrayerRequestCreateRequest createRequest = new PrayerRequestCreateRequest(1, "Suffering from headaches",
+                "Getting headaches, pray for healing", LocalDateTime.now().plusMonths(1), LocalDateTime.now());
 
-        assertThatExceptionOfType(DataValidationException.class).isThrownBy(() -> prayerRequestService.createPrayerRequest("mockAuthHeader", mockPrayerGroup.getPrayerGroupId(), createRequest));
+        Assertions.assertThatExceptionOfType(DataValidationException.class).isThrownBy(() -> prayerRequestService.createPrayerRequest("mockAuthHeader", mockPrayerGroup.getPrayerGroupId(), createRequest));
     }
 
+    @Test
+    @DirtiesContext
+    public void createPrayerRequest_givenValidRequest_createsPrayerRequest() {
+        PrayerGroup mockPrayerGroup = new PrayerGroup("Gryzzl Prayer Group", "A prayer group for Gryzzl", null,
+                VisibilityLevel.PRIVATE, null, null);
+        prayerGroupRepository.save(mockPrayerGroup);
+
+        User user = new User("Mike Bean", "mbean", "mbean@gryzzl.com", "mockPasswordHash", Role.USER);
+        userRepository.save(user);
+
+        Mockito.when(jwtService.extractTokenFromAuthHeader(anyString())).thenReturn("mockToken");
+        Mockito.when(jwtService.extractUserId("mockToken")).thenReturn(user.getUserId());
+
+        PrayerGroupUser prayerGroupUser = new PrayerGroupUser(user, mockPrayerGroup, PrayerGroupRole.ADMIN);
+        prayerGroupUserRepository.save(prayerGroupUser);
+
+        PrayerRequestCreateRequest createRequest = new PrayerRequestCreateRequest(1, "Suffering from headaches",
+                "Getting headaches, pray for healing", LocalDateTime.now().plusMonths(1), LocalDateTime.now());
+
+        PrayerRequestCreateResult prayerRequestCreateResult = new PrayerRequestCreateResult(787,
+                createRequest.getRequestTitle(), createRequest.getRequestDescription(),
+                createRequest.getCreatedDate(), createRequest.getExpirationDate(), mockPrayerGroup.getPrayerGroupId()
+                , mockPrayerGroup.getGroupName(), null, null, null, null, user.getUserId(), user.getUsername(),
+                user.getFullName(), null, null, null, null);
+
+        Mockito.when(prayerRequestJdbcRepository.createPrayerRequest(any())).thenReturn(prayerRequestCreateResult);
+
+        PrayerRequestModel prayerRequestModel = prayerRequestService.createPrayerRequest("mockAuthHeader", mockPrayerGroup.getPrayerGroupId(), createRequest);
+
+        Assertions.assertThat(prayerRequestModel.getPrayerRequestId()).isEqualTo(prayerRequestCreateResult.getPrayerRequestId());
+        Assertions.assertThat(prayerRequestModel.getRequestTitle()).isEqualTo(prayerRequestCreateResult.getRequestTitle());
+    }
 
 }
