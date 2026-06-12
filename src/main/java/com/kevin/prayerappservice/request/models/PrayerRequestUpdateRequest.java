75d222ae5782adff83fe57f33751d5896213ce8a
kdu2030 <kdu2030@gmail.com>
2026-06-12 10:51:25 -0400

    Add Prayer Request Update Request

---
+++ src/main/java/com/kevin/prayerappservice/request/models/PrayerRequestUpdateRequest.java
@@ -0,0 +1,41 @@
+package com.kevin.prayerappservice.request.models;
+
+import java.time.OffsetDateTime;
+
+public class PrayerRequestUpdateRequest {
+    private String requestTitle;
+    private String requestDescription;
+    private OffsetDateTime expirationDate;
+
+    public PrayerRequestUpdateRequest(){}
+
+    public PrayerRequestUpdateRequest(String requestTitle, String requestDescription, OffsetDateTime expirationDate) {
+        this.requestTitle = requestTitle;
+        this.requestDescription = requestDescription;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getRequestTitle() {
+        return requestTitle;
+    }
+
+    public void setRequestTitle(String requestTitle) {
+        this.requestTitle = requestTitle;
+    }
+
+    public String getRequestDescription() {
+        return requestDescription;
+    }
+
+    public void setRequestDescription(String requestDescription) {
+        this.requestDescription = requestDescription;
+    }
+
+    public OffsetDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(OffsetDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+}
