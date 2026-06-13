1e7456a3e84ed4c10a43850c8817d3baab7d45cb
kdu2030 <kdu2030@gmail.com>
2026-06-13 18:43:06 -0400

    Add Delete Prayer Request

---
+++ src/main/resources/db/migration/V2_48__delete_prayer_request.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE PROCEDURE delete_prayer_request (
+    p_prayer_request_id INT
+)
+AS
+$$
+BEGIN
+    DELETE FROM
+        prayer_request_session prs
+    WHERE
+        prs.prayer_request_id = p_prayer_request_id;
+
+    DELETE FROM
+        prayer_request_like prl
+    WHERE
+        prl.prayer_request_id = p_prayer_request_id;
+
+    DELETE FROM
+        prayer_request_comment prc
+    WHERE
+        prc.prayer_request_id = p_prayer_request_id;
+
+    DELETE FROM
+        prayer_request_bookmark prb
+    WHERE
+        prb.prayer_request_id = p_prayer_request_id;
+
+    DELETE FROM
+        prayer_request pr
+    WHERE
+        pr.prayer_request_id = p_prayer_request_id;
+
+    COMMIT;
+EXCEPTION
+    WHEN OTHERS THEN
+        ROLLBACK;
+        RAISE;
+END;
+$$
+LANGUAGE plpgsql;
